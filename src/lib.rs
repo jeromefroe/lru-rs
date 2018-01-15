3cff72776b4de4a4bd61564736e40ebf493a2047
Jerome Froelich <jeromefroe@uber.com>
2018-01-15 09:53:16 -0500

    Fix typos

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 // SOFTWARE.
 
 //! An implementation of a LRU cache. The cache supports `get`, `get_mut`, `put`,
-//! and `remove` operations, all of which are O(1). This crate was heavily influenced
+//! and `pop` operations, all of which are O(1). This crate was heavily influenced
 //! by the [LRU Cache implementation in an earlier version of Rust's std::collections crate] (https://doc.rust-lang.org/0.12.0/std/collections/lru_cache/struct.LruCache.html).
 //!
 //! ## Example
