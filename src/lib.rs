bcd57224a5158e6ec7eb1f3bb3cc65a8863055ff
Jerome Froelich <jeromefroelich@hotmail.com>
2026-08-27 16:29:14 -0400

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -417,10 +417,10 @@ impl<K: Hash + Eq, V, S: BuildHasher> LruCache<K, V, S> {
             (Some(replaced), old_node)
         } else {
             // if the cache is not full allocate a new LruEntry
-(
-    None,
-    NonNull::new(Box::into_raw(Box::new(LruEntry::new(k, v)))).unwrap(),
-)
+            (
+                None,
+                NonNull::new(Box::into_raw(Box::new(LruEntry::new(k, v)))).unwrap(),
+            )
         }
     }
 
